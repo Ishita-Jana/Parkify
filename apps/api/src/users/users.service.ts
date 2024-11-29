578fd9c99805b33b45e76c54957b81ba94ea19fe
Ishita <ishitamun2000@gmail.com>
2024-11-29 06:14:38 +0530

    changes made according to lint file

--- apps/api/src/users/users.service.ts
+++ apps/api/src/users/users.service.ts
@@ -1,34 +1,36 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserInput } from './dto/create-user.input';
-import { UpdateUserInput } from './dto/update-user.input';
-import { PrismaService } from 'src/util/prisma';
+import { Injectable } from '@nestjs/common'
+import { CreateUserInput } from './dto/create-user.input'
+import { UpdateUserInput } from './dto/update-user.input'
+import { PrismaService } from 'src/util/prisma'
 
 @Injectable()
 export class UsersService {
-
   constructor(private readonly prisma: PrismaService) {}
   create(createUserInput: CreateUserInput) {
-    return this.prisma.user.create({ data: createUserInput });
+    return this.prisma.user.create({ data: createUserInput })
     // 'This action adds a new user';
   }
 
   findAll() {
-    return this.prisma.user.findMany(); 
+    return this.prisma.user.findMany()
     // `This action returns all users`;
   }
 
   findOne(id: string) {
-    return this.prisma.user.findUnique({ where: { uid: id } });
+    return this.prisma.user.findUnique({ where: { uid: id } })
     // `This action returns a #${id} user`;
   }
 
   update(id: string, updateUserInput: UpdateUserInput) {
-    return this.prisma.user.update({ where: { uid: id }, data: updateUserInput }); 
+    return this.prisma.user.update({
+      where: { uid: id },
+      data: updateUserInput,
+    })
     // `This action updates a #${id} user`;
   }
 
   remove(id: string) {
-    return this.prisma.user.delete({ where: { uid: id } }); 
+    return this.prisma.user.delete({ where: { uid: id } })
     // `This action removes a #${id} user`;
   }
 }
--- libs/sample-lib/dist/index.js
+++ libs/sample-lib/dist/index.js
@@ -1,9 +1,6 @@
-'use strict'
-Object.defineProperty(exports, '__esModule', { value: true })
-exports.add = void 0
-const add = (a, b) => a + b
-exports.add = add
-console.log('hello')
-// "build": "yarn nx run-many -t build",
-//     "validate": "yarn format:write && yarn tsc && yarn lint && yarn build",
-//     "prepare": "husky install",
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.add = void 0;
+const add = (a, b) => a + b;
+exports.add = add;
+console.log('hello');
